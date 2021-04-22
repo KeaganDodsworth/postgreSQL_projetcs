7dd5d9159f384a5496d9cfc855947b5f88606ef4
Keagan Dodsworth <you@example.comkeagan.dods@gmail.com>
2021-04-22 14:02:51 +0200

    Touch Up

--- try_it_yourself_queries.sql
+++ try_it_yourself_queries.sql
@@ -1,3 +1,4 @@
+--Chapter 1
 CREATE TABLE animals (
 	id bigserial,
 	animal varchar(50)
@@ -45,13 +46,306 @@ CREATE TABLE char_data_types (
  text_column text
 );
 
-INSERT INTO char_data_types
-VALUES
- ('abc', 'abc', 'abc'),
- ('defghi', 'defghi', 'defghi');
- 
-COPY char_data_types TO 'C:\Users\Y520\Documents\postgreSQL\typetest.txt'
-WITH (FORMAT CSV, HEADER, DELIMITER '|');
+
+-- Chapter 2
+
+SELECT school, first_name, last_name
+FROM teachers
+ORDER BY school, last_name;
+
+SELECT first_name, last_name, school, salary
+FROM teachers
+WHERE first_name LIKE 'S%'
+      AND salary > 40000;
+
+
+SELECT last_name, first_name, school, hire_date, salary
+FROM teachers
+WHERE hire_date >= '2010-01-01'
+ORDER BY salary DESC;
+
+-- Chapter 3
+
+-- numeric(4,1)
+-- varchar(50)
+-- Error
+
+
+-- Chapter 4
+
+COPY actors
+FROM 'C:\Users\Keagan Dodsworth\Documents\postgre SQL\movies.txt'
+WITH (FORMAT CSV, HEADER, DELIMITER ':', QUOTE '#');
+
+CREATE TABLE actors (
+    id integer,
+    movie text,
+    actor text
+);
+
+CREATE TABLE us_counties_2010 (
+    geo_name varchar(90),                    -- Name of the geography
+    state_us_abbreviation varchar(2),        -- State/U.S. abbreviation
+    summary_level varchar(3),                -- Summary Level
+    region smallint,                         -- Region
+    division smallint,                       -- Division
+    state_fips varchar(2),                   -- State FIPS code
+    county_fips varchar(3),                  -- County code
+    area_land bigint,                        -- Area (Land) in square meters
+    area_water bigint,                        -- Area (Water) in square meters
+    population_count_100_percent integer,    -- Population count (100%)
+    housing_unit_count_100_percent integer,  -- Housing Unit count (100%)
+    internal_point_lat numeric(10,7),        -- Internal point (latitude)
+    internal_point_lon numeric(10,7),        -- Internal point (longitude)
+
+    -- This section is referred to as P1. Race:
+    p0010001 integer,   -- Total population
+    p0010002 integer,   -- Population of one race:
+    p0010003 integer,       -- White Alone
+    p0010004 integer,       -- Black or African American alone
+    p0010005 integer,       -- American Indian and Alaska Native alone
+    p0010006 integer,       -- Asian alone
+    p0010007 integer,       -- Native Hawaiian and Other Pacific Islander alone
+    p0010008 integer,       -- Some Other Race alone
+    p0010009 integer,   -- Population of two or more races
+    p0010010 integer,   -- Population of two races:
+    p0010011 integer,       -- White; Black or African American
+    p0010012 integer,       -- White; American Indian and Alaska Native
+    p0010013 integer,       -- White; Asian
+    p0010014 integer,       -- White; Native Hawaiian and Other Pacific Islander
+    p0010015 integer,       -- White; Some Other Race
+    p0010016 integer,       -- Black or African American; American Indian and Alaska Native
+    p0010017 integer,       -- Black or African American; Asian
+    p0010018 integer,       -- Black or African American; Native Hawaiian and Other Pacific Islander
+    p0010019 integer,       -- Black or African American; Some Other Race
+    p0010020 integer,       -- American Indian and Alaska Native; Asian
+    p0010021 integer,       -- American Indian and Alaska Native; Native Hawaiian and Other Pacific Islander
+    p0010022 integer,       -- American Indian and Alaska Native; Some Other Race
+    p0010023 integer,       -- Asian; Native Hawaiian and Other Pacific Islander
+    p0010024 integer,       -- Asian; Some Other Race
+    p0010025 integer,       -- Native Hawaiian and Other Pacific Islander; Some Other Race
+    p0010026 integer,   -- Population of three races
+    p0010047 integer,   -- Population of four races
+    p0010063 integer,   -- Population of five races
+    p0010070 integer,   -- Population of six races
+
+    -- This section is referred to as P2. HISPANIC OR LATINO, AND NOT HISPANIC OR LATINO BY RACE
+    p0020001 integer,   -- Total
+    p0020002 integer,   -- Hispanic or Latino
+    p0020003 integer,   -- Not Hispanic or Latino:
+    p0020004 integer,   -- Population of one race:
+    p0020005 integer,       -- White Alone
+    p0020006 integer,       -- Black or African American alone
+    p0020007 integer,       -- American Indian and Alaska Native alone
+    p0020008 integer,       -- Asian alone
+    p0020009 integer,       -- Native Hawaiian and Other Pacific Islander alone
+    p0020010 integer,       -- Some Other Race alone
+    p0020011 integer,   -- Two or More Races
+    p0020012 integer,   -- Population of two races
+    p0020028 integer,   -- Population of three races
+    p0020049 integer,   -- Population of four races
+    p0020065 integer,   -- Population of five races
+    p0020072 integer,   -- Population of six races
+
+    -- This section is referred to as P3. RACE FOR THE POPULATION 18 YEARS AND OVER
+    p0030001 integer,   -- Total
+    p0030002 integer,   -- Population of one race:
+    p0030003 integer,       -- White alone
+    p0030004 integer,       -- Black or African American alone
+    p0030005 integer,       -- American Indian and Alaska Native alone
+    p0030006 integer,       -- Asian alone
+    p0030007 integer,       -- Native Hawaiian and Other Pacific Islander alone
+    p0030008 integer,       -- Some Other Race alone
+    p0030009 integer,   -- Two or More Races
+    p0030010 integer,   -- Population of two races
+    p0030026 integer,   -- Population of three races
+    p0030047 integer,   -- Population of four races
+    p0030063 integer,   -- Population of five races
+    p0030070 integer,   -- Population of six races
+
+    -- This section is referred to as P4. HISPANIC OR LATINO, AND NOT HISPANIC OR LATINO BY RACE
+    -- FOR THE POPULATION 18 YEARS AND OVER
+    p0040001 integer,   -- Total
+    p0040002 integer,   -- Hispanic or Latino
+    p0040003 integer,   -- Not Hispanic or Latino:
+    p0040004 integer,   -- Population of one race:
+    p0040005 integer,   -- White alone
+    p0040006 integer,   -- Black or African American alone
+    p0040007 integer,   -- American Indian and Alaska Native alone
+    p0040008 integer,   -- Asian alone
+    p0040009 integer,   -- Native Hawaiian and Other Pacific Islander alone
+    p0040010 integer,   -- Some Other Race alone
+    p0040011 integer,   -- Two or More Races
+    p0040012 integer,   -- Population of two races
+    p0040028 integer,   -- Population of three races
+    p0040049 integer,   -- Population of four races
+    p0040065 integer,   -- Population of five races
+    p0040072 integer,   -- Population of six races
+
+    -- This section is referred to as H1. OCCUPANCY STATUS
+    h0010001 integer,   -- Total housing units
+    h0010002 integer,   -- Occupied
+    h0010003 integer    -- Vacant
+);
+
+SELECT * FROM us_counties_2010;
+
+COPY (
+    SELECT geo_name, state_us_abbreviation, housing_unit_count_100_percent
+    FROM   us_counties_2010 
+	ORDER BY housing_unit_count_100_percent 
+	DESC LIMIT 20
+     )
+TO 'C:\Users\Keagan Dodsworth\Documents\postgre SQL\us_counties_housing_export.txt'
+WITH (FORMAT CSV, HEADER);
+
+
+-- Chapter 5
+
+SELECT 3.14 * 5 ^ 2;
+
+SELECT 3.14 * (5 ^ 2);
+
+SELECT geo_name,
+       state_us_abbreviation,
+       p0010001 AS total_population,
+       p0010005 AS american_indian_alaska_native_alone,
+       (CAST (p0010005 AS numeric(8,1)) / p0010001) * 100
+           AS percent_american_indian_alaska_native_alone
+FROM us_counties_2010
+WHERE state_us_abbreviation = 'NY'
+ORDER BY percent_american_indian_alaska_native_alone DESC;
+
+
+SELECT percentile_cont(.5)
+        WITHIN GROUP (ORDER BY p0010001)
+FROM us_counties_2010
+WHERE state_us_abbreviation = 'NY';
+
+SELECT percentile_cont(.5)
+        WITHIN GROUP (ORDER BY p0010001)
+FROM us_counties_2010
+WHERE state_us_abbreviation = 'CA';
+
+SELECT state_us_abbreviation,
+       percentile_cont(0.5)
+          WITHIN GROUP (ORDER BY p0010001) AS median
+FROM us_counties_2010
+WHERE state_us_abbreviation IN ('NY', 'CA')
+GROUP BY state_us_abbreviation;
+
+
+SELECT state_us_abbreviation,
+       percentile_cont(0.5)
+          WITHIN GROUP (ORDER BY p0010001) AS median
+FROM us_counties_2010
+GROUP BY state_us_abbreviation;
+
+-- Chapter 6
+
+CREATE TABLE us_counties_2000 (
+    geo_name varchar(90),              -- County/state name,
+    state_us_abbreviation varchar(2),  -- State/U.S. abbreviation
+    state_fips varchar(2),             -- State FIPS code
+    county_fips varchar(3),            -- County code
+    p0010001 integer,                  -- Total population
+    p0010002 integer,                  -- Population of one race:
+    p0010003 integer,                      -- White Alone
+    p0010004 integer,                      -- Black or African American alone
+    p0010005 integer,                      -- American Indian and Alaska Native alone
+    p0010006 integer,                      -- Asian alone
+    p0010007 integer,                      -- Native Hawaiian and Other Pacific Islander alone
+    p0010008 integer,                      -- Some Other Race alone
+    p0010009 integer,                  -- Population of two or more races
+    p0010010 integer,                  -- Population of two races
+    p0020002 integer,                  -- Hispanic or Latino
+    p0020003 integer                   -- Not Hispanic or Latino:
+);
+
+COPY us_counties_2000
+FROM 'C:\Users\Keagan Dodsworth\Documents\postgre SQL\us_counties_2000.csv'
+WITH (FORMAT CSV, HEADER);
+
+SELECT c2010.geo_name,
+       c2010.state_us_abbreviation,
+       c2000.geo_name
+FROM us_counties_2010 c2010 LEFT JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips AND c2010.county_fips = c2000.county_fips
+WHERE c2000.geo_name IS NULL;
+
+SELECT c2010.geo_name,
+       c2000.geo_name,
+       c2000.state_us_abbreviation
+FROM us_counties_2010 c2010 RIGHT JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips AND c2010.county_fips = c2000.county_fips
+WHERE c2010.geo_name IS NULL;
+
+
+SELECT median(round( (CAST(c2010.p0010001 AS numeric(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 )) AS median_pct_change
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips AND c2010.county_fips = c2000.county_fips;
+   
+
+SELECT percentile_cont(.5)
+       WITHIN GROUP (ORDER BY round( (CAST(c2010.p0010001 AS numeric(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 )) AS percentile_50th
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips AND c2010.county_fips = c2000.county_fips;
+
+
+SELECT c2010.geo_name,
+       c2010.state_us_abbreviation,
+       c2010.p0010001 AS pop_2010,
+       c2000.p0010001 AS pop_2000,
+       c2010.p0010001 - c2000.p0010001 AS raw_change,
+       round( (CAST(c2010.p0010001 AS DECIMAL(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 ) AS pct_change
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips AND c2010.county_fips = c2000.county_fips
+ORDER BY pct_change ASC;
+
+-- Chapter 7
+
+CREATE TABLE albums (
+    album_id bigserial,
+    album_catalog_code varchar(100),
+    album_title text,
+    album_artist text,
+    album_time interval,
+    album_release_date date,
+    album_genre varchar(40),
+    album_description text
+);
+
+CREATE TABLE songs (
+    song_id bigserial,
+    song_title text,
+    song_artist text,
+    album_id bigint
+);
+
+CREATE TABLE albums (
+    album_id bigserial,
+    album_catalog_code varchar(100) NOT NULL,
+    album_title text NOT NULL,
+    album_artist text NOT NULL,
+    album_release_date date,
+    album_genre varchar(40),
+    album_description text,
+    CONSTRAINT album_id_key PRIMARY KEY (album_id),
+    CONSTRAINT release_date_check CHECK (album_release_date > '1/1/1925')
+);
+
+CREATE TABLE songs (
+    song_id bigserial,
+    song_title text NOT NULL,
+    song_artist text NOT NULL,
+    album_id bigint REFERENCES albums (album_id),
+    CONSTRAINT song_id_key PRIMARY KEY (song_id)
+);
+
+-- Chapter 8
 
 SELECT pls14.stabr,
        sum(pls14.gpterms) AS gpterms_2014,
@@ -64,7 +358,6 @@ WHERE pls14.gpterms >= 0 AND pls09.gpterms >= 0
 GROUP BY pls14.stabr
 ORDER BY pct_change DESC;
 
-
 SELECT pls14.stabr,
        sum(pls14.pitusr) AS pitusr_2014,
        sum(pls09.pitusr) AS pitusr_2009,
@@ -75,6 +368,9 @@ ON pls14.fscskey = pls09.fscskey
 WHERE pls14.pitusr >= 0 AND pls09.pitusr >= 0
 GROUP BY pls14.stabr
 ORDER BY pct_change DESC;
+
+
+-- a) 
 
 SELECT pls14.obereg,
        sum(pls14.visits) AS visits_2014,
@@ -87,83 +383,43 @@ WHERE pls14.visits >= 0 AND pls09.visits >= 0
 GROUP BY pls14.obereg
 ORDER BY pct_change DESC;
 
+-- b) 
+
+CREATE TABLE obereg_codes (
+    obereg varchar(2) CONSTRAINT obereg_key PRIMARY KEY,
+    region varchar(50)
+);
+
+INSERT INTO obereg_codes
+VALUES ('01', 'New England (CT ME MA NH RI VT)'),
+       ('02', 'Mid East (DE DC MD NJ NY PA)'),
+       ('03', 'Great Lakes (IL IN MI OH WI)'),
+       ('04', 'Plains (IA KS MN MO NE ND SD)'),
+       ('05', 'Southeast (AL AR FL GA KY LA MS NC SC TN VA WV)'),
+       ('06', 'Soutwest (AZ NM OK TX)'),
+       ('07', 'Rocky Mountains (CO ID MT UT WY)'),
+       ('08', 'Far West (AK CA HI NV OR WA)'),
+       ('09', 'Outlying Areas (AS GU MP PR VI)');
+
+
+
+SELECT obereg_codes.region,
+       sum(pls14.visits) AS visits_2014,
+       sum(pls09.visits) AS visits_2009,
+       round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+                    sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+   ON pls14.fscskey = pls09.fscskey
+JOIN obereg_codes
+   ON pls14.obereg = obereg_codes.obereg
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY obereg_codes.region
+ORDER BY pct_change DESC;
+
+-- 3
+
 SELECT pls14.libname, pls14.city, pls14.stabr, pls14.statstru, pls14.c_admin, pls14.branlib,
        pls09.libname, pls09.city, pls09.stabr, pls09.statstru, pls09.c_admin, pls09.branlib
 FROM pls_fy2014_pupld14a pls14 FULL OUTER JOIN pls_fy2009_pupld09a pls09
 ON pls14.fscskey = pls09.fscskey
 WHERE pls14.fscskey IS NULL OR pls09.fscskey IS NULL;
-
-CREATE TABLE meat_poultry_egg_inspect (
-    est_number varchar(50) CONSTRAINT est_number_key PRIMARY KEY,
-    company varchar(100),
-    street varchar(100),
-    city varchar(30),
-    st varchar(2),
-    zip varchar(5),
-    phone varchar(14),
-    grant_date date,
-    activities text,
-    dbas text
-);
-
-COPY meat_poultry_egg_inspect
-FROM 'C:\Users\Keagan Dodsworth\Documents\postgre SQL\MPI_Directory_by_Establishment_Name.csv'
-WITH (FORMAT CSV, HEADER, DELIMITER ',');
-
-CREATE INDEX company_idx ON meat_poultry_egg_inspect (company);
-
--- Count the rows imported:
-SELECT count(*) FROM meat_poultry_egg_inspect;
-
-SELECT est_number,
- company,
- city, 
- st,
- zip
-FROM meat_poultry_egg_inspect
-WHERE st IS NULL;
-
-
-SELECT est_number,
- company,
- city, 
- st,
- zip
-FROM meat_poultry_egg_inspect
-WHERE zip = '55449'AND city =  'Blaine';
-
-UPDATE meat_poultry_egg_inspect
-SET st = 'MN'
-Where est_number = 'V18677A';
-
-CREATE TABLE state_regions (
-    st varchar(2) CONSTRAINT st_key PRIMARY KEY,
-    region varchar(20) NOT NULL
-);
-
-COPY state_regions
-FROM 'C:\Users\Keagan Dodsworth\Documents\postgre SQL\state_regions.csv'
-WITH (FORMAT CSV, HEADER, DELIMITER ',');
-
-ALTER TABLE meat_poultry_egg_inspect ADD COLUMN meat_processing boolean;
-ALTER TABLE meat_poultry_egg_inspect ADD COLUMN poultry_processing boolean;
-
-SELECT * FROM meat_poultry_egg_inspect; 
-
-UPDATE meat_poultry_egg_inspect
-SET meat_processing = TRUE
-WHERE activities ILIKE '%meat processing%';
-
-UPDATE meat_poultry_egg_inspect
-SET poultry_processing = TRUE
-WHERE activities ILIKE '%poultry processing%'; 
-
-SELECT * FROM meat_poultry_egg_inspect;
-
-SELECT count(meat_processing), count(poultry_processing)
-FROM meat_poultry_egg_inspect;
-
-SELECT count(*)
-FROM meat_poultry_egg_inspect
-WHERE meat_processing = TRUE AND
-      poultry_processing = TRUE;
